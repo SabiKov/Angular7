393ed911be4249bb186e567fce4a19906f885f68
SabiKov <skovacs33@gmail.com>
2019-02-26 23:38:38 +0100

    added template and selectors options

--- bootstrap-site/src/app/servers/servers.component.ts
+++ bootstrap-site/src/app/servers/servers.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  // styleUrls: ['./servers.component.css']
+  styles: [`
+    h1 {
+      color: yellow;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
