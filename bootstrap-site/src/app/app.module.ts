1b7c96fcb7233642b3d4b4bedf1e1441fa26c785
SabiKov <skovacs33@gmail.com>
2019-03-02 21:09:31 +0100

    added data binding

--- bootstrap-site/src/app/app.module.ts
+++ bootstrap-site/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningalertComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule
