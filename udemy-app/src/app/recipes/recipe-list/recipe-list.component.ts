bd1af0194f04db9f855237053b0791d89d854760
SabiKov <skovacs33@gmail.com>
2019-03-10 23:04:47 +0100

    added all basic components and logic

--- udemy-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,11 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recepies: Recipe[] = [
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a simple test',
+      'https://get.pxhere.com/photo/dish-meal-food-' +
+      'produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-' +
+      'chop-power-dishes-grilling-fried-food-604134.jpg'),
     new Recipe('A test recipe', 'This is a simple test',
       'https://get.pxhere.com/photo/dish-meal-food-' +
       'produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-' +
